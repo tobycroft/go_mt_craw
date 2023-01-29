6877ec09269ec2a131e701389ded8effa7d37578
火线兔 <silverhawk@foxmail.com>
2023-01-29 17:08:31 +0800

    TuuzMac

--- go.mod
+++ go.mod
@@ -5,10 +5,27 @@ go 1.19
 require github.com/anaskhan96/soup v1.2.5
 
 require (
+	github.com/PuerkitoBio/goquery v1.5.1 // indirect
+	github.com/andybalholm/cascadia v1.2.0 // indirect
+	github.com/antchfx/htmlquery v1.2.3 // indirect
+	github.com/antchfx/xmlquery v1.2.4 // indirect
+	github.com/antchfx/xpath v1.1.8 // indirect
+	github.com/gobwas/glob v0.2.3 // indirect
+	github.com/gocolly/colly/v2 v2.1.0 // indirect
+	github.com/golang/groupcache v0.0.0-20200121045136-8c9f03a8e57e // indirect
+	github.com/golang/protobuf v1.4.2 // indirect
+	github.com/json-iterator/go v1.1.12 // indirect
+	github.com/kennygrant/sanitize v1.2.4 // indirect
+	github.com/modern-go/concurrent v0.0.0-20180228061459-e0a39a4cb421 // indirect
+	github.com/modern-go/reflect2 v1.0.2 // indirect
+	github.com/saintfish/chardet v0.0.0-20120816061221-3af4cd4741ca // indirect
 	github.com/shopspring/decimal v1.3.1 // indirect
+	github.com/temoto/robotstxt v1.1.1 // indirect
 	github.com/tobycroft/Calc v1.2.2 // indirect
 	github.com/tobycroft/HttpRequest v1.1.5 // indirect
 	github.com/tobycroft/TuuzNet v1.0.0 // indirect
-	golang.org/x/net v0.0.0-20200114155413-6afb5195e5aa // indirect
-	golang.org/x/text v0.3.0 // indirect
+	golang.org/x/net v0.0.0-20200602114024-627f9648deb9 // indirect
+	golang.org/x/text v0.3.2 // indirect
+	google.golang.org/appengine v1.6.6 // indirect
+	google.golang.org/protobuf v1.24.0 // indirect
 )
--- main.go
+++ main.go
@@ -2,24 +2,50 @@ package main
 
 import (
 	"fmt"
-	Net "github.com/tobycroft/TuuzNet"
+	"github.com/gocolly/colly/v2"
+	"strings"
 )
 
 func main() {
-	query := map[string]interface{}{
-		"technicianId": 1000000,
-	}
-	ua := map[string]string{
-		"User-Agent": "PostmanRuntime/7.30.0"}
-	resp, err := Net.Get("https://g.meituan.com/domino/craftsman-app/craftsman-detail.html", query, ua, nil)
-	if err != nil {
-		fmt.Println(err)
-		return
-	}
-	fmt.Println(resp)
-	//doc := soup.HTMLParse(resp)
-	//links := doc.Find("div", "id", "comicLinks").FindAll("a")
-	//for _, link := range links {
-	//	fmt.Println(link.Text(), "| Link :", link.Attrs()["href"])
+	//query := map[string]interface{}{
+	//	"technicianId": 1000000,
 	//}
+	//ua := map[string]string{
+	//	"User-Agent": "PostmanRuntime/7.30.0"}
+	//resp, err := Net.Get("https://g.meituan.com/domino/craftsman-app/craftsman-detail.html", query, ua, nil)
+	//if err != nil {
+	//	fmt.Println(err)
+	//	return
+	//}
+	//fmt.Println(resp)
+	////doc := soup.HTMLParse(resp)
+	////links := doc.Find("div", "id", "comicLinks").FindAll("a")
+	////for _, link := range links {
+	////	fmt.Println(link.Text(), "| Link :", link.Attrs()["href"])
+	////}
+
+	c := colly.NewCollector()
+
+	// Find and visit all links
+	c.OnHTML("a[href]", func(e *colly.HTMLElement) {
+		e.Request.Visit(e.Attr("href"))
+	})
+
+	c.OnRequest(func(r *colly.Request) {
+		fmt.Println("Visiting", r.URL)
+	})
+
+	c.OnResponse(func(e *colly.Response) {
+		body := string(e.Body)
+		bodys1 := strings.Split(body, "window.__INITIAL_STATE__ = ")
+		bodys2 := bodys1[len(bodys1)-1]
+		bodys3 := strings.Split(bodys2, "</script>")
+		bodys4 := bodys3[0]
+		s6 := strings.TrimSpace(bodys4)
+		//jsoniter.UnmarshalFromString(s6)
+		fmt.Println(s6)
+	})
+
+	c.Visit("https://g.meituan.com/domino/craftsman-app/craftsman-detail.html?technicianId=11728812")
+
 }
